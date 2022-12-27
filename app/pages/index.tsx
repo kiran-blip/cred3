70d8ee57be00096e14058803007b903ed27476f8
Kiran Nedumkallel <knedumkallel@gmail.com>
2022-12-27 21:52:50 +0000

    Initial Frontend

---
+++ app/pages/index.tsx
@@ -0,0 +1,14 @@
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.main}>
+        <h1 className={styles.title}>
+          Proof of Solana dev{" "}
+          {/* <a href="https://alchemy.com/solana/?a=d0c917f7ef">Alchemy</a>! */}
+        </h1>
+      </div>
+    </div>
+  );
+}
